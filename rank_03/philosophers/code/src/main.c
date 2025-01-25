200b8669d834e054c1bf69e3eb01b7d2ecb82aa4
Jin1 <jin1@iMac-von-TJ.local>
2025-01-25 22:22:47 +0100

    updated

--- rank_03/philosophers/code/src/main.c
+++ rank_03/philosophers/code/src/main.c
@@ -17,14 +17,14 @@ void	display_data(t_data *d)
 	printf("* * * display starts... * * *\n");
 
 	printf("nbr_of_philos: %zu\n", d->nbr_of_philos);
-	printf("time_to_die: %lu\n", d->time_to_die);
-	printf("time_to_eat: %lu\n", d->time_to_eat);
-	printf("time_to_sleep: %lu\n", d->time_to_sleep);
+	printf("time_to_die: %llu\n", d->time_to_die);
+	printf("time_to_eat: %llu\n", d->time_to_eat);
+	printf("time_to_sleep: %llu\n", d->time_to_sleep);
 	printf("nbr_of_times_each_philo_must_eat: %zu\n", d->nbr_of_times_each_philo_must_eat);
 
 	printf("nbr_of_philos_full: %d\n", d->nbr_of_philos_full);
 	printf("dead_philo_id: %d\n", d->dead_philo_id);
-	printf("start_time: %lu\n", d->start_time);
+	printf("start_time: %llu\n", d->start_time);
 
 	printf("* * * printing philosophers * * *\n");
 	for (size_t i = 0; i < d->nbr_of_philos; i++)
@@ -32,8 +32,8 @@ void	display_data(t_data *d)
 		printf("philos[%zu].id: %d\n", i, d->philos[i].id);
 		printf("philos[%zu].has_died: %d\n", i, d->philos[i].has_died);
 		printf("philos[%zu].meal_count: %d\n", i, d->philos[i].meal_count);
-		printf("philos[%zu].last_meal_time: %lu\n", i, d->philos[i].last_meal_time);
-		printf("philos[%zu].death_timestamp: %lu\n", i, d->philos[i].death_timestamp);
+		printf("philos[%zu].last_meal_time: %llu\n", i, d->philos[i].last_meal_time);
+		printf("philos[%zu].death_timestamp: %llu\n", i, d->philos[i].death_timestamp);
 	}
 	printf("* * * display ends... * * *\n");
 }
